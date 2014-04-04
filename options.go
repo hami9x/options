1646bbb9982d0950049c40c7dad84cc1a0eccc47
phaikawl <phaikawl@gmail.com>
2014-04-05 00:33:41 +0700

    It's working.

---
+++ options.go
@@ -0,0 +1,92 @@
+//Package options implements a simple, flexible and convenient method for specifying options,
+//with the help of reflections.
+package options
+
+import (
+	"fmt"
+	"reflect"
+)
+
+//Spec specifies the list of options.
+//A spec should be implemented as a struct which have Option fields,
+//each representing an option.
+//Each Option must be a different struct, multiple fields with the same struct
+//type will cause unwanted behavior (only the last field receives the value).
+type Spec interface{}
+
+//Option represents an option.
+//Each option must be a different struct, and should be defined
+//as a struct with a Value field.
+//
+//For example:
+//	type RouteName struct { Value: string }
+type Option interface{}
+
+type OptionsProvider struct {
+	spec Spec
+	m    map[string]string
+}
+
+//NewOptions creates a new OptionsProvider.
+func NewOptions(spec Spec) *OptionsProvider {
+	o := &OptionsProvider{spec, make(map[string]string)}
+	v := reflect.ValueOf(spec)
+	if v.CanSet() {
+		panic(fmt.Sprintf("The spec passed in must be a pointer, got type %v",
+			v.Type().Name()))
+	}
+	v = v.Elem()
+	specType := v.Type()
+	for i := 0; i < specType.NumField(); i++ {
+		field := specType.Field(i)
+		o.m[field.Type.Name()] = field.Name
+	}
+	return o
+}
+
+//Options set the options in spec according to the opts passed in.
+func (o *OptionsProvider) Options(opts ...Option) *OptionsProvider {
+	for _, opt := range opts {
+		optType := reflect.TypeOf(opt)
+		if _, ok := optType.FieldByName("Value"); !ok {
+			panic(fmt.Sprintf("Option %v doesn't have a Value field.", optType.Name()))
+		}
+		field := reflect.ValueOf(o.spec).Elem().FieldByName(o.m[optType.Name()])
+		if !field.CanSet() || !field.IsValid() {
+			panic(fmt.Sprintf("There is no option %v.", optType.Name()))
+		}
+		field.Set(reflect.ValueOf(opt))
+	}
+	return o
+}
+
+//Get returns the options data, which could be casted
+//to the original spec type and the value could be get from there.
+func (o *OptionsProvider) Get() interface{} {
+	return o.spec
+}
+
+//ExportToMap exports the options to a map
+func (o *OptionsProvider) ExportToMap() map[string]interface{} {
+	return o.ExportToMapWithTag("")
+}
+
+//ExportToMapWithTag exports the options to a map,
+//the key names in the map are determined by a specific struct tag.
+//The tags are set in the options spec.
+func (o *OptionsProvider) ExportToMapWithTag(tag string) map[string]interface{} {
+	spec := reflect.ValueOf(o.spec).Elem()
+	specType := spec.Type()
+	m := make(map[string]interface{})
+	for i := 0; i < specType.NumField(); i++ {
+		exportedName := specType.Field(i).Name
+		if tag != "" {
+			taggedName := specType.Field(i).Tag.Get(tag)
+			if taggedName != "" {
+				exportedName = taggedName
+			}
+		}
+		m[exportedName] = spec.Field(i).FieldByName("Value").Interface()
+	}
+	return m
+}
---
+++ options_test.go
@@ -0,0 +1,45 @@
+package options
+
+import (
+	"testing"
+	. "launchpad.net/gocheck"
+)
+
+//An option
+type WhateverName struct{ Value string }
+
+//An option
+type WhateverNum struct{ Value int }
+
+//RouteSpec specifies the options
+type WhateverSpec struct {
+	Name WhateverName `js:"name"`
+	Num  WhateverNum  `js:"num"`
+}
+
+func WhateverOptions(opts ...Option) *OptionsProvider {
+	return NewOptions(&WhateverSpec{}).Options(opts...)
+}
+
+func GetWhateverOptions(o *OptionsProvider) *WhateverSpec {
+	return o.Get().(*WhateverSpec)
+}
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestRouteOptions(c *C) {
+	name, num := "n0t9r34t6cz...", 999999
+	opts := WhateverOptions(
+		WhateverName{name},
+		WhateverNum{num},
+	)
+	m := opts.ExportToMapWithTag("js")
+	c.Check(GetWhateverOptions(opts).Name.Value, Equals, name)
+	c.Check(m["name"].(string), Equals, name)
+	c.Check(m["num"].(int), Equals, num)
+}
